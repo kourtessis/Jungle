a3d8fb7756e53a382157e0157453b991dff695e5
Krista Kourtessis <krista.kourtessis@telus.com>
2023-01-25 15:28:33 -0800

    admin dashboard-count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,14 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: Rails.configuration.admin[:user],
   password: Rails.configuration.admin[:password]
   def show
-    
+    @product_count=Product.all.count 
+    @category_count=Category.all.count 
+    products=Product.all
+
+    @item_count = 0
+
+    products.each do |item|
+      @item_count = @item_count.to_i + item.quantity.to_i 
+    end
   end
 end
