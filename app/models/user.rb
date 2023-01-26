45377dcfdaaef69f4dd5fe9a1414b09354a45946
Krista Kourtessis <krista.kourtessis@telus.com>
2023-01-26 11:14:30 -0800

    implement user login/signup pages - all functional

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+# class User < ApplicationRecord
+class User < ActiveRecord::Base
+  has_secure_password
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+end
