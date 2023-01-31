9874ac68a9cc1f827b2842839690b46e4ac5a813
Krista Kourtessis <krista.kourtessis@telus.com>
2023-01-31 09:41:56 -0800

    update cypress file and run tests

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,14 @@
+describe('example to-do app', () => {
+  it('visit base url', () => {
+    cy.visit('http://localhost:3000/')
+  })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 12 products on the page", () => {
+    cy.get(".products article").should("have.length", 12);
+  });
+
+})
