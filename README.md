fdb216a99d870d889def5fd3fefc2e8540231e47
Krista Kourtessis <krista.kourtessis@telus.com>
2023-01-31 14:26:26 -0800

    screenshots to github

--- README.md
+++ README.md
@@ -29,3 +29,10 @@ More information in their docs: <https://stripe.com/docs/testing#cards>
 - Bootstrap 5
 - PostgreSQL 9.x
 - Stripe
+
+## Screenshots
+!["Home page"]()
+
+!["Home page"]()
+
+!["Home page"]()
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,10 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
+
       redirect_to '/'
     else
-      redirect_to '/users/new'
+      redirect_to '/login'
     end
   end
 
